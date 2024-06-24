3ca55fbde2ceca2ac4cbf3b6637e84390c698ee4
unknown <kaushikyash262@gmail.com>
2024-06-24 11:34:52 +0530

    Vercel Deploy

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use("/uploads", express.static(__dirname + '/uploads'));
 app.use(
   cors({
     credentials: true,
-    origin: ["https://rent-nest-ruby.vercel.app/","http://localhost:5173"],
+    origin: ["https://rent-nest-p3tmjw0ud-yash-kaushiks-projects.vercel.app","http://localhost:5173"],
     methods: ["POST", "PUT", "GET", "DELETE"],
   })
 );
